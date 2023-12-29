74117357891dfcfda681ac4f5372ed67d8d45acc
kimdh <kdh.devs@gmail.com>
2023-12-29 23:06:34 +0900

    23/12/29 반응형 사이즈, 모바일 메뉴, ABOUT ME 수정

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,9 +1,10 @@
 const btn_more = document.querySelector('.intro-more');
-const btn_about_me = document.querySelector('.AboutMe');
-const btn_skill = document.querySelector('.Skills');
-const btn_project = document.querySelector('.Projects');
-const btn_career = document.querySelector('.Career');
-const btn_education = document.querySelector('.Education');
+
+const btn_about_me = document.querySelectorAll('.AboutMe');
+const btn_skill = document.querySelectorAll('.Skills');
+const btn_project = document.querySelectorAll('.Projects');
+const btn_career = document.querySelectorAll('.Career');
+const btn_education = document.querySelectorAll('.Education');
 
 const height_intro = document.querySelector('.intro').offsetHeight;
 const div_header = document.querySelector('.header');
@@ -17,39 +18,108 @@ const div_phone_num = document.querySelector('.phone-num');
 const div_email = document.querySelector('.email');
 const div_edu = document.querySelector('.edu');
 
+const header = document.querySelector('header');
+
+let menu_state = false;
+let is_mobile = false;
+let win_width = 0;
+
+function win_resized(w_size) {
+    if(w_size < 950) {
+        is_mobile = true;
+        header.classList.add('mobile');
+        div_title.classList.add('mobile');
+    } else {
+        is_mobile = false;
+        $('.sub-menu-mobile').css('display', 'none');
+        header.classList.remove('mobile');
+        div_title.classList.remove('mobile');
+    }
+}
+
+function menu_animation(state) {
+    if(state) {
+        $('.sub-menu-mobile').slideDown(200);
+        menu_state = true;
+    } else {
+        $('.sub-menu-mobile').slideUp(200);
+        menu_state = false;
+    }
+}
+
+$(document).ready(function () {
+    win_width = $(window).width();
+    win_resized(win_width);
+
+
+    $(window).on('resize', function () {
+        win_width = $(window).width();
+        win_resized(win_width);
+    });
+
+    $('.open-menu').click(function () {
+        menu_animation(!menu_state);
+    });
+});
+
 btn_more.addEventListener('click', function () {
     gsap.to(window, 0, {
         scrollTo: ".aboutme"
     });
 });
 
-btn_about_me.addEventListener('click', function () {
-    gsap.to(window, 0, {
-        scrollTo: ".aboutme"
+btn_about_me.forEach(menu_item => {
+    menu_item.addEventListener('click', function () {
+        gsap.to(window, 0, {
+            scrollTo: ".aboutme"
+        });
+        if(is_mobile) {
+            menu_animation(false);
+        }
     });
 });
 
-btn_skill.addEventListener('click', function () {
-    gsap.to(window, 0, {
-        scrollTo: ".skill"
+btn_skill.forEach(menu_item => {
+    menu_item.addEventListener('click', function () {
+        gsap.to(window, 0, {
+            scrollTo: ".skill"
+        });
+        if(is_mobile) {
+            menu_animation(false);
+        }
     });
 });
 
-btn_project.addEventListener('click', function () {
-    gsap.to(window, 0, {
-        scrollTo: ".project"
+btn_project.forEach(menu_item => {
+    menu_item.addEventListener('click', function () {
+        gsap.to(window, 0, {
+            scrollTo: ".project"
+        });
+        if(is_mobile) {
+            menu_animation(false);
+        }
     });
 });
 
-btn_career.addEventListener('click', function () {
-    gsap.to(window, 0, {
-        scrollTo: ".career"
+btn_career.forEach(menu_item => {
+    menu_item.addEventListener('click', function () {
+        gsap.to(window, 0, {
+            scrollTo: ".career"
+        });
+        if(is_mobile) {
+            menu_animation(false);
+        }
     });
 });
 
-btn_education.addEventListener('click', function () {
-    gsap.to(window, 0, {
-        scrollTo: ".education"
+btn_education.forEach(menu_item => {
+    menu_item.addEventListener('click', function () {
+        gsap.to(window, 0, {
+            scrollTo: ".education"
+        });
+        if(is_mobile) {
+            menu_animation(false);
+        }
     });
 });
 
@@ -76,7 +146,7 @@ window.addEventListener('scroll', _.throttle(
 const encoded_name = 'JUVBJUI5JTgwJUVCJThGJTk5JUVEJTlDJTk4';
 const encoded_birth = 'MDMuMDguMzA=';
 const encoded_address = 'JUVBJUIyJUJEJUVBJUI4JUIwJUVCJThGJTg0JTIwJUVCJUI2JTgwJUVDJUIyJTlDJUVDJThCJTlD';
-const encoded_phone_num = 'KzgyJTIwMTAtNDMxOS0yMzc4';
+const encoded_phone_num = 'MDEwLTQzMTktMjM3OA==';
 const encoded_email = 'a2RoLmRldnNAZ21haWwuY29t';
 const encoded_edu = 'JUVDJTlEJUI4JUVDJUIyJTlDJUVDJUEwJTg0JUVDJTlFJTkwJUVCJUE3JTg4JUVDJTlEJUI0JUVDJThBJUE0JUVEJTg0JUIwJUVBJUIzJUEwJTIwKCVFQyVBMCU4NCVFQyU5RSU5MCVFQyVBMCU5QyVFQyU5NiVCNCVFQSVCMyVCQyk=';
 
@@ -95,5 +165,5 @@ div_phone_num.textContent = decoded_phone_num;
 div_email.textContent = decoded_email;
 div_edu.textContent = decoded_edu;
 
-
-
+div_phone_num.setAttribute('href',`tel:${decoded_phone_num}`);
+div_email.setAttribute('href',`mailto:${decoded_email}`);
